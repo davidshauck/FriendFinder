c35673a2b80afec5ef1be74ca01f37f863439917
David Hauck <dhauck@mac.com>
2019-12-22 17:59:58 -0500

    working verion with modal and calculation of best friend

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+let friendsArray = [
+    {
+    name: "Leo",
+    photo: "https://www.goldenglobes.com/sites/default/files/styles/portrait_medium/public/people/cover_images/leonardo_dicaprio-gt.jpg?itok=uZBLZv3X",
+    answers:[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
+  },
+  {
+    name: "Brad",
+    photo: "https://www.onthisday.com/images/people/brad-pitt-medium.jpg",
+    answers:[
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2
+      ]
+  },
+  {
+    name: "George",
+    photo: "https://www.onthisday.com/images/people/george-clooney-medium.jpg",
+    answers:[
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  }
+];
+
+  module.exports = friendsArray;
